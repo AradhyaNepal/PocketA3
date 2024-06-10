3b2f6e17b490035355d3165b86f11da33831357c
Aradhya Nepal <aradhya.1221@gmail.com>
2024-06-10 21:54:43 +0545

    Working with registering user

--- SprintBootBackend/src/main/java/com/a2/pocketA3/features/auth/dto/RegisterResendOTPRequest.java
+++ SprintBootBackend/src/main/java/com/a2/pocketA3/features/auth/dto/RegisterResendOTPRequest.java
@@ -2,7 +2,6 @@ package com.a2.pocketA3.features.auth.dto;
 
 import com.a2.pocketA3.constants.ValidationConstants;
 import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
--- SprintBootBackend/src/main/java/com/a2/pocketA3/features/auth/entity/RegisteringUser.java
+++ SprintBootBackend/src/main/java/com/a2/pocketA3/features/auth/entity/RegisteringUser.java
@@ -1,22 +1,32 @@
 package com.a2.pocketA3.features.auth.entity;
-
+import com.a2.pocketA3.features.auth.enums.GenderType;
 import com.a2.pocketA3.features.auth.enums.MBTIType;
+import com.a2.pocketA3.features.auth.enums.MaritalStatus;
+import com.a2.pocketA3.features.auth.enums.Occasion;
 import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Data;
 
-import java.lang.annotation.ElementType;
 import java.time.LocalDate;
 
+@Builder
+@Data
 public class RegisteringUser {
     @Id
     @GeneratedValue
     private int id;
     private String email;
     private String fullName;
-    private String profileUrl;
-    private String gender;
     private LocalDate birthDate;
     private String country;
+    private String phoneNumber;
+
+    private MaritalStatus maritalStatus;
+    private GenderType gender;
+    private Occasion occasion;
     private MBTIType mbtiType;
+
+    private String profileUrl;
     private double heightCM;
     private double weightKg;
     private double fatPercentage;
