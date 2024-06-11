a06b4629d7f5a21ea1fc388527c00c28af095a71
Aradhya Nepal <aradhya.1221@gmail.com>
2024-06-11 20:31:26 +0545

    Working with api

--- SprintBootBackend/src/main/java/com/a2/pocketA3/features/auth/controller/RegisterController.java
+++ SprintBootBackend/src/main/java/com/a2/pocketA3/features/auth/controller/RegisterController.java
@@ -1,14 +1,53 @@
 package com.a2.pocketA3.features.auth.controller;
 
+import com.a2.pocketA3.features.auth.dto.*;
 import com.a2.pocketA3.features.auth.services.RegisterService;
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+@RequestMapping(value = "register")
 @RestController
 @AllArgsConstructor
 public class RegisterController {
 
     private RegisterService registerService;
+
+    @PostMapping(value = "s1-add-email")
+    public void registerEmail(@RequestBody @Valid RegisterEmailRequest request) {
+
+    }
+
+    @PostMapping(value = "s2-verify-otp")
+    public void verifyOTP(@RequestBody @Valid RegisterEmailRequest request) {
+
+    }
+
+    @PostMapping(value = "s2-resend-otp")
+    public void resendOTP(@RequestBody @Valid RegisterResendOTPRequest request) {
+
+    }
+
+
+    @PostMapping(value = "s3-first-details")
+    public void setFirstDetails(@RequestBody @Valid RegisterFirstDetailsRequest request) {
+
+    }
+
+    @PostMapping(value = "s4-second-details")
+    public void setSecondDetails(@RequestBody @Valid RegisterSecondDetailsRequest request) {
+
+    }
+
+    @PostMapping(value = "s5-third-details")
+    public void setThirdDetails(@RequestBody @Valid RegisterFinalDetailsRequest request) {
+
+    }
+
+    @PostMapping(value = "s6-set-password")
+    public void setPassword(@RequestBody @Valid RegisterSetPasswordRequest request) {
+
+    }
 
 
 }
